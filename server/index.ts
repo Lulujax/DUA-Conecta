afd84902cfc0aa790287ba551b46a22b812c7098
Luis <luispenaponce@gmail.com>
2025-10-29 23:27:15 -0400

    cambios en origin

--- server/index.ts
+++ server/index.ts
@@ -21,7 +21,7 @@ const app = new Elysia()
    .use(cors({
     // ¡CAMBIO IMPORTANTE AQUÍ!
     // Reemplaza '<TU_URL_DE_VERCEL>' con tu URL real (ej: 'https://dua-conecta.vercel.app')
-        origin: ['http://localhost:5173', 'https://dua-conecta-j1pn-a2gbn28h8-lulujaxs-projects.vercel.app'], 
+        origin: ['http://localhost:5173', 'https://dua-conecta-j1pn.vercel.app'], 
         methods: ['GET', 'POST', 'PUT', 'OPTIONS'],
         allowedHeaders: ['Content-Type', 'Authorization'],
         credentials: true,
